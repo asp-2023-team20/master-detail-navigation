d04a53b7697b878a68675f4973642a9b6fc18eb4
Kyle <elvewyn@gmail.com>
2023-08-12 12:01:25 +0800

    example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useParams, useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+
+
+// generate 100 recipes
+const recipes = Array.from(Array(100).keys());
+
+const RecipeIndex = () => {
+  let params = useParams();
+
+  return <div className='relative flex flex-col block h-screen w-screen overflow-clip'>
+    <h1 className='text-xl font-bold leading-8'>Recipe</h1>
+    <div className='overflow-auto'>
+      <ul key='abc'>
+        {recipes.map(r => <li key={`r{r}`} ><Link to={`${r}`}>Recipe {r}</Link></li>)}
+      </ul >
+    </div >
+
+    {params.id && <RecipeDetail id={params.id} />}
+
+  </div >;
+}
+
+const RecipeDetail = ({ id }) => {
+  let navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate('/recipe');
+  };
+
+  return <div className='absolute left-0 top-0 z-10 h-screen w-screen block bg-white'>
+    <div className="flex flex-col ">
+      <div>
+        <button onClick={handleBack}>Back</button>
+      </div>
+      <div>
+        <h1 className='text-xl font-bold'>Detail {id}</h1>
+      </div>
+    </div>
+  </div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes>
+    <Route path="/recipe" name="home" element={<RecipeIndex />} />
+    <Route path="/recipe/:id" element={<RecipeIndex />} />
+  </Routes>
 }
 
 export default App;
